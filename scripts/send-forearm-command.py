4830cf1ff4cc743ce9646a0296d4b8e38579361c
Wolfgang Merkt <w.merkt@gmail.com>
2016-04-20 16:11:42 +0100

    Update send forearm command

--- scripts/send-forearm-command.py
+++ scripts/send-forearm-command.py
@@ -8,11 +8,14 @@ import sys
 myLCM = lcm.LCM()
 
 msg = lcmbotcore.joint_angles_t()
-msg.joint_name = ["rightForearmYaw"]
+msg.joint_name = ["rightForearmYaw", "rightWristRoll", "rightWristPitch"]
 
 msg.num_joints = len(msg.joint_name)
 msg.joint_position = [0] * len(msg.joint_name)
 
 msg.joint_position[0] = float(sys.argv[1])
+msg.joint_position[1] = float(sys.argv[2])
+msg.joint_position[2] = float(sys.argv[3])
+
 print msg.joint_name[0] + " going to " + str(msg.joint_position[0])
 myLCM.publish("DESIRED_FOREARM_ANGLES", msg.encode())

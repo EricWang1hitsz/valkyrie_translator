3753b28125a2a32b7c06c9f648a7e300c51a6413
Wolfgang Merkt <w.merkt@gmail.com>
2016-04-20 16:14:01 +0100

    Add safety send forearm command and make side a variable

--- scripts/send-forearm-command.py
+++ scripts/send-forearm-command.py
@@ -5,10 +5,15 @@ import bot_core as lcmbotcore
 import time
 import sys
 
+side = "left"
+
+assert side in ["left", "right"]
+assert len(sys.argv) == 4
+
 myLCM = lcm.LCM()
 
 msg = lcmbotcore.joint_angles_t()
-msg.joint_name = ["rightForearmYaw", "rightWristRoll", "rightWristPitch"]
+msg.joint_name = [side + "ForearmYaw", side + "WristRoll", side + "WristPitch"]
 
 msg.num_joints = len(msg.joint_name)
 msg.joint_position = [0] * len(msg.joint_name)
@@ -18,4 +23,6 @@ msg.joint_position[1] = float(sys.argv[2])
 msg.joint_position[2] = float(sys.argv[3])
 
 print msg.joint_name[0] + " going to " + str(msg.joint_position[0])
+print msg.joint_name[1] + " going to " + str(msg.joint_position[1])
+print msg.joint_name[2] + " going to " + str(msg.joint_position[2])
 myLCM.publish("DESIRED_FOREARM_ANGLES", msg.encode())
